81a387d33841e8cdd94b57a3416d5bb66b228dd5
iLBigga <Giannanti1996@gmail.com>
2022-10-16 13:00:33 +0200

    Creata tabella comics

--- database/migrations/2022_10_16_104700_create_comics_table.php
+++ database/migrations/2022_10_16_104700_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 2,2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
