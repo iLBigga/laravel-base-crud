af563d76c7de2321b4490b8d2a54ee9b21a1a8e5
iLBigga <Giannanti1996@gmail.com>
2022-10-17 15:31:06 +0200

    Refactory

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,17 +12,19 @@
     <div class="container">
         <ul>
             @foreach ($comics as $c)
-            <h2>{{ $c->title }}</h2>
-            <a href="{{ route('comics.show', $c) }}">
-                <img src="{{ $c->thumb }}" alt="{{ $c->title }}">
-            </a>
-            <a href="{{ route('comics.edit', $c) }}">Modifica</a>
-            <form action="{{ route('comics.destroy', $c) }}" method="POST">
-                @csrf
-                @method('DELETE')
-
-                <input type="submit" value="Elimina">
-            </form>
+            <div class="card" style="border-bottom: 1px solid black;">
+                <h2>{{ $c->title }}</h2>
+                <a href="{{ route('comics.show', $c) }}">
+                    <img src="{{ $c->thumb }}" alt="{{ $c->title }}">
+                </a>
+                <a style="display: block;" href="{{ route('comics.edit', $c) }}">Modifica</a>
+                <form action="{{ route('comics.destroy', $c) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+    
+                    <input type="submit" value="Elimina">
+                </form>
+            </div>
             @endforeach
         </ul>
     </div>
